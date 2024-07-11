9e9750badc2e425725ee3477e91bcf4c95bb509d
Hüseyin Ardıl Karasungur <72706704+karasungurhuseyinardil@users.noreply.github.com>
2024-07-11 09:47:39 +0300

    Cart Class Created

--- ecommerce.java
+++ ecommerce.java
@@ -69,3 +69,27 @@ class Product {
         this.description = description;
     }
 }
+class Cart {
+    private Map<Product, Integer> items;
+
+    public Cart() {
+        this.items = new HashMap<>();
+    }
+
+    public void addProduct(Product product, int quantity) {
+        items.put(product, items.getOrDefault(product, 0) + quantity);
+    }
+
+    public void removeProduct(Product product) {
+        items.remove(product);
+    }
+
+    public double getTotalPrice() {
+        return items.entrySet().stream().mapToDouble(e -> e.getKey().getPrice() * e.getValue()).sum();
+    }
+
+    public Map<Product, Integer> getItems() {
+        return items;
+    }
+}
+
